e1df9d96ed805c5de87ca4bef366bd19f88f9dcf
kutsenkoid <kutsenkoid@mer.ci.nsu.ru>
2026-02-11 14:32:28 +0700

    add compose

--- main/src/main/java/ci/nsu/moble/main/MainActivity2.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity2.kt
@@ -1,18 +1,47 @@
 package ci.nsu.moble.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ci.nsu.moble.main.ui.main.MainFragment
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ci.nsu.moble.main.ui.theme.PracticeTheme
 
-class MainActivity2 : AppCompatActivity() {
-
+class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+        enableEdgeToEdge()
+        setContent {
+            PracticeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticeTheme {
+        Greeting("Android")
     }
 }
